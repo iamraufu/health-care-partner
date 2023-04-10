879ff28330c494ebf078d9f66454b83f3374047e
iamraufu <raufu.dev@gmail.com>
2023-04-11 00:36:39 +0600

    [UPDATED]

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import hospitalData from '../data/hospital.json'
 
 const Contact = () => {
     return (
         <div className='bg-brand'>
             <Navbar />
-            Contact
+            <div className="container">
+                <div className="row mt-5">
+                    <h2 className="fs-5 text-brand">Hospital Information</h2>
+                    <table className="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th style={{ fontSize: '13px' }}>Name</th>
+                                <th style={{ fontSize: '13px' }}>Address</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {
+                                hospitalData.length > 0 &&
+                                hospitalData.map((data, index) =>
+                                    <tr style={{ border: '1px solid lightgray' }} key={index}>
+                                        <td style={{ width: '117px' }}>{data.name}</td>
+                                        <td style={{ width: '117px' }}>{data.address}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
